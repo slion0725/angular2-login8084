3c7b73d32022372d5eec39fda21e323d34b51bc9
miles <slion0725@gmail.com>
2016-12-21 15:56:05 +0800

    jquery bootstrat set

--- src/app/simple/simple.component.ts
+++ src/app/simple/simple.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $:any;
+
 @Component({
   selector: 'app-simple',
   templateUrl: './simple.component.html',
@@ -10,6 +12,7 @@ export class SimpleComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log($('body'));
   }
 
 }
